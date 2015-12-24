ccf4f8f9c0422d18abc3de0c1c21037744cbef51
liqiang <2484232069@qq.com>
2015-12-24 22:13:00 +0800

    news

--- src/main/java/com/news/controller/NewsController.java
+++ src/main/java/com/news/controller/NewsController.java
@@ -1,10 +1,13 @@
 package com.news.controller;
 
+import com.news.service.NewsService;
 import org.apache.tomcat.jni.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +18,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class NewsController {
     private static final Logger LOGGER = LoggerFactory.getLogger(NewsController.class);
+    @Autowired
+    private NewsService newsService;
 
     @RequestMapping("newsAdd")
     public ModelAndView newsAdd(ModelAndView modelAndView){
@@ -30,18 +35,17 @@ public class NewsController {
         return modelAndView;
     }
 
-    @RequestMapping("newsPostAdd")
+    @RequestMapping(value = "newsPostAdd" , method = RequestMethod.POST)
     @ResponseBody
     public String newsPostAdd(@RequestParam String title, @RequestParam String content, @RequestParam File file){
         LOGGER.debug("view is news post add method");
         return "";
     }
 
-    @RequestMapping("")
+    @RequestMapping(value = "findAllByLike" , method = RequestMethod.POST)
     @ResponseBody
     public String findAllByLike() {
         LOGGER.debug("view is news find by condition or all");
         return "";
-
     }
 }
