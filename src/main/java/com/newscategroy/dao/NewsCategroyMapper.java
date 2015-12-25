ad89127b1b4260f57bbfd302d044b928f51dfa15
周家梁 <zhoujialiang@zhoujialiangdeMacBook-Air.local>
2015-12-25 11:52:41 +0800

    zhou

---
+++ src/main/java/com/newscategroy/dao/NewsCategroyMapper.java
@@ -0,0 +1,12 @@
+package com.newscategroy.dao;
+
+import com.base.mapper.SqlMapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by dianjinzi on 24/12/15.
+ */
+@Component
+public interface NewsCategroyMapper extends SqlMapper{
+
+}
--- src/main/java/com/user/dao/UserMapper.java
+++ src/main/java/com/user/dao/UserMapper.java
@@ -2,13 +2,15 @@ package com.user.dao;
 
 import com.base.mapper.SqlMapper;
 import com.user.bean.User;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 /**
  * Created by dianjinzi on 22/12/15.
  */
 
-
+@Component
 public interface UserMapper extends SqlMapper{
 
      List<User> findAll();
