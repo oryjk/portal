8f25aaa8d28a8137b4e14f1e69d83c7b87975ee4
liqiang <2484232069@qq.com>
2015-12-29 18:58:15 +0800

    news

---
+++ src/main/webapp/resources/core/js/news/newsOperator.js
@@ -0,0 +1,35 @@
+﻿//$(function(){
+//    var options={
+//        url:"fileUpload",
+//        type:"post",
+//        success:function(mes){
+//            $("#show").empty();
+//            $("#show").append("<img src=' <%=basePath%>" + mes + " '/>");
+//        }
+//    };
+//    $("#uploadForm").submit(function() {
+//        $(this).ajaxSubmit(options);
+//        return false;
+//    });
+//
+//    $("#image_submit").click(function(){
+//        $("#uploadForm").submit();
+//    });
+//
+//    $("#submit").click(function(){
+//        var title = $("#title").val();
+//        var content = $("#content").val();
+//        var type = $("#type").val();
+//
+//        $.post("newsPostAdd",
+//            {
+//                "title":title,
+//                "content":content,
+//                "type":type
+//            },
+//            function(data){
+//                layer.msg(data);
+//                window.location.reload();
+//            });
+//    })
+//});
